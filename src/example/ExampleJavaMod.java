f74018ccaab5b63f8d4e34fa8a2e219e2d1c591e
StesNiash <111519914+StesNiash@users.noreply.github.com>
2026-05-02 03:42:31 +0300

    Теперь работает корректно и протестирован

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,7 +1,11 @@
 package example;
 
+import arc.*;
+import arc.struct.*;
 import arc.util.*;
+import mindustry.*;
 import mindustry.content.*;
+import mindustry.game.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
@@ -9,15 +13,33 @@ import mindustry.mod.*;
 public class ExampleJavaMod extends Mod {
 
     public ExampleJavaMod() {
+        Events.run(Trigger.update, () -> {
+            if (Vars.state != null && Vars.state.isGame()) {
+                for (Teams.TeamData data : Vars.state.teams.present) {
+                    Queue<Teams.BlockPlan> plans = data.plans;
+                    int size = plans.size;
+                    for (int i = 0; i < size; i++) {
+                        Teams.BlockPlan plan = plans.get(i);
+                        if (!plan.removed && plan.block == Blocks.coreShard) {
+                            plan.removed = true;
+                            plans.addLast(new Teams.BlockPlan(plan.x, plan.y, plan.rotation, Blocks.vault, plan.config));
+                        }
+                    }
+                }
+            }
+        });
+
         Events.on(BlockBuildBeginEvent.class, e -> {
-            if (!e.breaking && e.block == Blocks.coreShard) {
-                Call.constructBegin(e.tile, Blocks.vault, e.unit);
+            if (!e.breaking) {
+                Building build = e.tile.build;
+                if (build != null && build.block == Blocks.coreShard) {
+                    build.block = Blocks.vault;
+                }
             }
         });
     }
 
     @Override
     public void loadContent() {
-        Log.info("Core-shard to vault replacer mod loaded.");
     }
 }

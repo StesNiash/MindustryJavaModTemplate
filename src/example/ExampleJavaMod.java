2fdad80174f055b8cd906bce0f937aeec948cf3e
StesNiash <111519914+StesNiash@users.noreply.github.com>
2026-06-10 12:09:08 +0300

    Main code implemented

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -2,32 +2,28 @@ package example;
 
 import arc.*;
 import arc.util.*;
+import mindustry.*;
 import mindustry.game.EventType.*;
+import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class ExampleJavaMod extends Mod {
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+    public ExampleJavaMod() {
+        Log.info("No Player Collision mod loaded.");
 
-        //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
+            Events.run(Trigger.update, () -> {
+                if (Vars.state.isGame() && Vars.player != null && Vars.player.unit() != null) {
+                    Vars.player.unit().type.physics = false;
+                }
             });
         });
     }
 
     @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
+    public void loadContent() {
+        Log.info("Loading no player collision content.");
     }
 
 }
